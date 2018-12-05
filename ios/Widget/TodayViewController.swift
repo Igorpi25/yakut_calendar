a0b5a07d8767bed1393870e9529251b1499e7035
Igor Ivanov <igorpi25@gmail.com>
2018-12-06 04:14:42 +0900

    Get plain text from Flutter Asset

--- ios/Widget/TodayViewController.swift
+++ ios/Widget/TodayViewController.swift
@@ -24,6 +24,8 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         let seconds = calendar.component(.second, from: date)
         
         widget_title.text = "\(hour):\(minutes):\(seconds)"
+        
+        getSummary()
 
     }
     
@@ -44,14 +46,49 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     func getSummary(){
         
-        let bundle = Bundle.main
+        let key = "flutter_assets/assets/2019/1/1"
+
+        let s = readDataFromFile(file: key)
         
-//        let key = FlutterPluginRegistrar
-//        lookupKeyForAsset("icons/heart.png")
+        print("Igor s="+s)
         
-        let path = bundle.path(forResource: key, ofType: "txt")
+        guard let data = s.data(using: String.Encoding.unicode) else { return }
         
+        try? widget_title.attributedText = NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil)
         
+    }
+    
+    func getSummaryTextFromHtml(html: String)->String{
+//        do{
+//            let doc: Document = try SwiftSoup.parse("...")
+//            let links: Elements = try doc.select("a[href]") // a with href
+//            let pngs: Elements = try doc.select("img[src$=.png]")
+//
+//            // img with src ending .png
+//            let masthead: Element? = try doc.select("div.masthead").first()
+//
+//            // div with class=masthead
+//            let resultLinks: Elements? = try doc.select("h3.r > a") // direct a after h3
+//        } catch Exception.Error(let type, let message){
+//            print(message)
+//        } catch {
+//            print("error")
+//        }
+        return "asdad"
+    }
+    
+    func readDataFromFile(file:String)-> String{
+        guard let filepath = Bundle.main.path(forResource: file, ofType: nil)
+            else {
+                return "Нету1"
+        }
+        do {
+            let contents = try String(contentsOfFile: filepath)
+            return contents
+        } catch {
+            print("File Read Error for file \(file)")
+            return "Нету2"
+        }
     }
     
 }
